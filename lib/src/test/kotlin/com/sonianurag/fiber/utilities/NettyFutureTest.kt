d946911d4329661cafd27c0fd4e4fe71247f9bd7
Anurag Soni <anurag@sonianurag.com>
2024-05-25 17:28:42 -0400

    netty: add tests for await

--- lib/src/test/kotlin/com/sonianurag/fiber/utilities/NettyFutureTest.kt
+++ lib/src/test/kotlin/com/sonianurag/fiber/utilities/NettyFutureTest.kt
@@ -1,19 +1,22 @@
 package com.sonianurag.fiber.utilities
 
 import io.netty.util.concurrent.DefaultEventExecutor
-import io.netty.util.concurrent.DefaultPromise
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 import kotlin.test.assertEquals
+
+class CustomException : Exception()
 
 class NettyFutureTest {
     @Test
     fun `can await on netty futures`() {
         runTest {
             val executor = DefaultEventExecutor()
-            val promise1 = DefaultPromise<Int>(executor)
-            val promise2 = DefaultPromise<Int>(executor)
+            val promise1 = executor.newPromise<Int>()
+            val promise2 = executor.newPromise<Int>()
             executor.submit {
                 promise1.setSuccess(1)
             }
@@ -25,6 +28,46 @@ class NettyFutureTest {
                 assertEquals(expected = 3, actual = promise1.coAwait() + promise2.coAwait())
             }
 
+            executor.shutdownGracefully().coAwait()
+        }
+    }
+
+    @Test
+    fun `await will propagate exceptions`() {
+        runTest {
+            val executor = DefaultEventExecutor()
+            val promise1 = executor.newPromise<Int>()
+            val promise2 = executor.newPromise<Int>()
+            executor.submit {
+                promise1.setSuccess(1)
+            }
+            executor.submit {
+                promise2.setFailure(CustomException())
+            }
+
+            assertThrows<CustomException> {
+                promise1.coAwait() + promise2.coAwait()
+            }
+            executor.shutdownGracefully().coAwait()
+        }
+    }
+
+    @Test
+    fun `await will propagate cancellations`() {
+        runTest {
+            val executor = DefaultEventExecutor()
+            val promise1 = executor.newPromise<Int>()
+            val promise2 = executor.newPromise<Int>()
+            executor.submit {
+                promise1.cancel(false)
+            }
+            executor.submit {
+                promise2.setSuccess(1)
+            }
+
+            assertThrows<CancellationException> {
+                promise1.coAwait() + promise2.coAwait()
+            }
             executor.shutdownGracefully().coAwait()
         }
     }
