4bb5a7843d3716839e5572fc3932b0fd4677fe18
Anurag Soni <anurag@sonianurag.com>
2024-06-07 22:21:57 -0400

    Add utility method to create requests

--- lib/src/main/kotlin/com/sonianurag/fiber/http/Request.kt
+++ lib/src/main/kotlin/com/sonianurag/fiber/http/Request.kt
@@ -16,4 +16,22 @@ interface Request {
 
     /** Body associated with the request. */
     val body: Body
+
+    companion object {
+        fun create(
+            version: Version,
+            method: Method,
+            uri: String,
+            headers: Headers = Headers(),
+            body: Body = Body.empty
+        ): Request {
+            return object : Request {
+                override val version: Version = version
+                override val method: Method = method
+                override val uri: String = uri
+                override val headers: Headers = headers
+                override val body: Body = body
+            }
+        }
+    }
 }
