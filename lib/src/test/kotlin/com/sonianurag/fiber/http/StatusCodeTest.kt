5386b996545e16d9a7c83658cd06dc8c76925969
Anurag Soni <anurag@sonianurag.com>
2024-06-28 22:35:03 -0400

    http: improve status code test

--- lib/src/test/kotlin/com/sonianurag/fiber/http/StatusCodeTest.kt
+++ lib/src/test/kotlin/com/sonianurag/fiber/http/StatusCodeTest.kt
@@ -1,23 +1,20 @@
 package com.sonianurag.fiber.http
 
-import io.netty.handler.codec.http.HttpResponseStatus
-import io.netty.handler.codec.http.HttpStatusClass
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class StatusCodeTest {
     @Test
     fun `verify that we match netty status class`() {
-        (200..600).forEach { code ->
-            val nettyCode = HttpResponseStatus.valueOf(code)
+        (200 ..< 600).forEach { code ->
             val fiberCode = StatusCode(code)
-            when (nettyCode.codeClass()) {
-                HttpStatusClass.SUCCESS -> assertTrue { fiberCode.isSuccess() }
-                HttpStatusClass.CLIENT_ERROR -> assertTrue { fiberCode.isClientError() }
-                HttpStatusClass.SERVER_ERROR -> assertTrue { fiberCode.isServerError() }
-                HttpStatusClass.INFORMATIONAL -> assertTrue { fiberCode.isInformational() }
-                HttpStatusClass.REDIRECTION -> assertTrue { fiberCode.isRedirection() }
-                else -> {}
+            when (code) {
+                in 100 ..< 200 -> assertTrue { fiberCode.isInformational() }
+                in 200 ..< 300 -> assertTrue { fiberCode.isSuccess() }
+                in 300 ..< 400 -> assertTrue { fiberCode.isRedirection() }
+                in 400 ..< 500 -> assertTrue { fiberCode.isClientError() }
+                in 500 ..< 600 -> assertTrue { fiberCode.isServerError() }
+                else -> TODO()
             }
         }
     }
